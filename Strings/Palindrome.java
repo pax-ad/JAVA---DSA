528d8e6d98fe2a1acf83516a5fbc8ba8cc3b0e71
pax-ad <ad007scrutinize@gmail.com>
2026-06-26 23:29:02 +0530

    IS PALindrome

---
+++ Strings/Palindrome.java
@@ -0,0 +1,26 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class Palindrome {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(isPalindrome(str));
+         sc.close();
+
+    }
+
+    public static boolean isPalindrome(String str){
+        for(int i = 0 ; i < str.length(); i++){
+            int n = str.length();
+            if( str.charAt(i) != str.charAt(n-1-i) ){
+                return false;
+            }
+        }
+        return true;
+
+    }
+    
+}
