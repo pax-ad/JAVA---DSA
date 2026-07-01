a279b5979af27f3214aff3a610ed2d8b8bdde7f8
pax-ad <ad007scrutinize@gmail.com>
2026-07-01 23:33:53 +0530

    Power to binary

---
+++ BitManipulation/FastExpo.java
@@ -0,0 +1,20 @@
+package BitManipulation;
+
+public class FastExpo {
+    
+    //programme for fast exponentiation
+    public static void main(String[] args) {
+        
+    }
+
+  //converting over power into binary
+  
+
+
+
+}
+
+
+
+
+
