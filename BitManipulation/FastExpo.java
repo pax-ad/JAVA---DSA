1edebc7f9114e5226b5ebc61cc08a466a9a94915
pax-ad <ad007scrutinize@gmail.com>
2026-07-02 18:57:10 +0530

    exponential power as binary

--- BitManipulation/FastExpo.java
+++ BitManipulation/FastExpo.java
@@ -4,11 +4,21 @@ public class FastExpo {
     
     //programme for fast exponentiation
     public static void main(String[] args) {
-        
+        System.out.println(fast_exponentiation(2, 10));
     }
 
   //converting over power into binary
-  
+  public static int fast_exponentiation(int a, int b) {
+    int ans = 1;
+    while(b > 0) {
+        if((b & 1) != 0) {
+            ans *= a;
+        }
+        a *= a;
+        b >>= 1;
+    }
+    return ans;
+  }
 
 
 
