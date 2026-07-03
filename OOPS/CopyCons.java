1c45fd124d90acdb3349d943f219c4c008bc28a6
pax-ad <ad007scrutinize@gmail.com>
2026-07-03 23:25:21 +0530

    copy constructor

---
+++ OOPS/CopyCons.java
@@ -0,0 +1,12 @@
+package OOPs;
+
+public class CopyCons {
+    
+    public static void main(String[] args) {
+        Student s1 = ne wStudent();
+        s1.name = "Aman";
+        s1.roll =456;
+        s1.password = "abcd"; 
+        s1.marks[0]=100;
+    }
+}
