f9debd1c0d30f0359f0e086384bd6bc1da15ede9
pax-ad <ad007scrutinize@gmail.com>
2026-07-04 17:56:34 +0530

    Constructor copy

--- OOPS/CopyCons.java
+++ OOPS/CopyCons.java
@@ -1,12 +1,40 @@
 package OOPs;
 
 public class CopyCons {
-    
+    //programe for copy constructor
     public static void main(String[] args) {
-        Student s1 = ne wStudent();
+        Student s1 = new Student();
         s1.name = "Aman";
         s1.roll =456;
         s1.password = "abcd"; 
         s1.marks[0]=100;
+        s1.marks[1]=90;
+
+        Student s2 = new Student(s1); //copy constructor is called
+        s2.password="xyz";
+        for(int i=0;i<3;i++){
+            System.out.println(s2.marks[i]);
+        }
+
+    }
+}
+
+class Student{
+    String name;
+    int roll;
+    String password;
+    int marks[];
+
+    Student(){
+        marks = new int[3];
+        System.out.println("Constructor is called");
+    }
+
+    //copy constructor
+    Student(Student s1){ //this is a shallow copy 
+        marks = new int[3]; //is actually wasted, because immediately afterwards you overwrite it: this.marks = s1.marks; //this is a shallow copy, because both objects will point to the same array in heap memory
+        this.name = s1.name;
+        this.roll = s1.roll;
+        this.marks = s1.marks;
     }
 }
