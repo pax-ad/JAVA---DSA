6dbbc2265fdf7fe6035768f91d6bdb8d4d4f5a21
pax-ad <ad007scrutinize@gmail.com>
2026-07-06 23:41:45 +0530

    added PracticeQ

---
+++ Practice /Q1.java
@@ -0,0 +1,14 @@
+
+
+public class Q1 {
+    //programme for reversing a array in place
+
+    public static void main(String[] args) {
+        int [] num = {1,2,3,4,5};
+        reverse(num);
+    }
+
+    public static void reverse(int[] num)
+    
+    }
+}
