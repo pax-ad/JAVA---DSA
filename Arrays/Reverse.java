5d24e8fedc0bb4d3f1bf7d58ee0faf01db7a1c12
pax-ad <ad007scrutinize@gmail.com>
2026-07-06 23:31:06 +0530

    Reverse array in place

--- Arrays/Reverse.java
+++ Arrays/Reverse.java
@@ -4,16 +4,23 @@ public class Reverse {
     
     public static void main (String [] args) {
         int[] num = {1,2,3,4,5,6,7,8,9,10};
-        Cycle(num);
+       Cycle(num);
         for (int i = 0 ; i < num.length; i++ ){
-            System.out.println("Your reversed array : " + num[i]);
+             System.out.println(num[i]);
         }
+
     }
 
     public static void  Cycle(int num[]){
-        int n = num.length;
-        int [] temp = new int[n];
-        4
+       int start = 0;
+       int last = num.length-1;
+       while(start<last){
+        int temp = num[last];
+        num[last] = num[start];
+        num[start] = temp;
+        start++;    
+        last--;
+       }
                
 
     }
