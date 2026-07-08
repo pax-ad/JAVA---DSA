1577b71e62e41dc399d96c80f97c2532ac1f7709
pax-ad <ad007scrutinize@gmail.com>
2026-07-09 00:07:02 +0530

    Array 2 sum

---
+++ Practice/Q4.java
@@ -0,0 +1,27 @@
+public class Q4 {
+    //two sum array 
+
+    //leetcode number 1
+
+    public static void main(String[] args) {
+        int num[]={2,7,11,15};
+        int target ;
+        Sum(num,9);
+    
+    }
+
+    public static void Sum(int num[],int target){
+        for(int i =0 ; i < num.length;i++){
+            for(int j= i+1;j<num.length;j++){
+                if(num[i]+num[j] == target){
+                     System.out.print("["+i+","+j+"]");
+                     return;
+                }
+            }
+            }
+
+          System.out.print("Not found");
+        }
+
+    }
+
