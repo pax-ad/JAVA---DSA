cb97d8af1da06797252ebfee2f806d5204203f21
pax-ad <ad007scrutinize@gmail.com>
2026-07-09 23:43:29 +0530

    binary search

---
+++ Practice/Q10.java
@@ -0,0 +1,36 @@
+package Practice;
+
+public class Q10 {
+
+    //binary search
+
+    public static void main(String[] args) {
+        int num[]={2,4,6,8,10,12,14};
+       System.out.println( BSearch(num, 10));
+    }
+
+    public static int BSearch(int num[], int key){
+        int start = 0 ;
+        int end = num.length-1;
+       
+        while(start<=end){
+             int mid = (start + end )/ 2;
+        
+            if(num[mid]==key){
+                return mid;
+            }
+            if(num[mid] < key){
+                start = mid + 1 ;
+
+            }
+
+            else{
+                end = mid - 1 ;
+            }
+        }
+        return -1;
+    
+    }
+
+}
+
