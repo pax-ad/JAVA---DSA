849ab35d1f7d0871b41812b7a774527dc87c982c
pax-ad <ad007scrutinize@gmail.com>
2026-07-10 00:12:43 +0530

    Array sorted

--- PracticeQ/Q11.java
+++ PracticeQ/Q11.java
@@ -5,17 +5,35 @@ public class Q11 {
 
     public static void main(String[] args) {
         int num[]={1,2,3,4,5};
-        Sorted(num);
+        System.err.println(Sorted(num));
+        System.out.println(Osorted(num));
     }
 
-    public static void Sorted(int num[]){
+    public static boolean Sorted(int num[]){
         for(int i = 0; i < num.length;i++){
             for(int j = i + 1 ; j <=num.length-1;j++){
                 if(num[i]<num[j]){
-                    System.out.println("array is  sorted");
+                    return true;
                 }
             }
         }
+        return false;
     }
+    //time compleaxity O(^2)
     
+ //optimal way
+ public static boolean Osorted(int num[]){
+    //edge case array having single element is always sorted
+     if (num == null || num.length <= 1) {
+        return true;
+    }
+
+
+    for(int i = 0 ; i < num.length-1;i++){
+        if(num[i]>num[i+1]){
+            return false;
+        }
+    }
+    return true; // edge case not covered in this bcz loop will treat it as null case.
+ }
 }
