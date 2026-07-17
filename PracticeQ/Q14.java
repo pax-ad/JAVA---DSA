9b74ffad1f20873292bdbbea203aca8d482f6334
pax-ad <ad007scrutinize@gmail.com>
2026-07-17 23:36:16 +0530

    bubble sort

---
+++ PracticeQ/Q14.java
@@ -0,0 +1,16 @@
+package PracticeQ;
+
+public class Q14 {
+    //bubble sorting practice
+
+    public static void main(String[] args) {
+        int [] num = { 2,7,8,5,3,0,9};
+
+    }
+
+    public static void Bubblesort(int num[]){
+        for(int i = 0 ; i < num.length();i++){
+            
+        }
+    }
+}
