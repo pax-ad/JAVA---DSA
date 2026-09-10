adf127be7f3e667f4888df08638593ad696150f6
pax-ad <ad007scrutinize@gmail.com>
2026-09-11 02:27:34 +0530

    BuyandSell Stocks

--- Arrays/Stock.java
+++ Arrays/Stock.java
@@ -1,5 +1,31 @@
 package Arrays;
 
 public class Stock {
-    
+    public static void main(String[] args) {
+        int prices [] ={7,1,5,3,6,6,4};
+        System.out.println(BuyandSell(prices));
+
+    }
+
+    public static int BuyandSell(int prices[]){
+
+        int buyingprince = Integer.MAX_VALUE; // + infinity
+        int Maxprofit = 0;
+
+        for(int i = 0 ; i < prices.length ; i++){
+            if(buyingprince > prices[i]){
+                //for future selling we willl buy stocks
+                buyingprince = prices[i];
+            }
+
+            else {
+                int Profit = prices[i] - buyingprince;
+                Maxprofit = Math.max(Maxprofit,Profit);
+            }
+            }
+            
+        
+        return Maxprofit;
+        
+    }
 }
