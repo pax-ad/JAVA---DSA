2c0cfd5e1a2fb5ce8ea573a333af0cd1c9269a02
pax-ad <ad007scrutinize@gmail.com>
2026-09-17 01:08:02 +0530

    Fix syntax error and add getter/setter to Access.java

--- OOPS/Access.java
+++ OOPS/Access.java
@@ -4,26 +4,27 @@ public class Access {
     public static void main(String[] args){
         BankAccount myAcc = new BankAccount();
         myAcc.username = "Aman";
-       // myAcc.password = "abcdefghi"; 
-        // this will give an error because password is private and can only be accessed within the class  
         
+        // 1. Set the private password using the setter method
         myAcc.setPassword("1234");
-        System.out.print( myAcc.setPassword("1234"));
+        
+        // 2. Read the private password using the getter method
+        System.out.print(myAcc.getPassword());
     }
-
-    
 }
 
-class BankAccount{
+class BankAccount {
     public String username;
     private String password;
 
-    //we need to make some changes in bankaccount
-    public String setPassword(String pwd){
-        password = pwd;
-        return pwd;
+    /*Since password is private, the class provides a public method public String setPassword(String pwd) to safely change it from the outside. */
+    // Setter: Used to modify the private variable
+    public void setPassword(String pwd) {
+        this.password = pwd;
+    }
+
+    // Getter: Used to safely read the private variable
+    public String getPassword() {
+        return this.password;
     }
 }
-
-
-    
